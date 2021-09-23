9a91a25ce293cf6b1222f4e190cb4e698147d553
vench juma <venchngogo22@gmail.com>
2021-09-23 11:10:10 +0300

    Created a new Expo app

--- task.js
+++ task.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-const Task = (props) => {
+const task = (props) => {
 
   return (
     <View style={styles.item}>
@@ -41,4 +41,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Task;
+export default task;
